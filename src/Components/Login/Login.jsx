b3735bbdade357e5b1b7ea13e694c2a2c3441862
Sawon-52 <mehedihasansawon.52@gmail.com>
2024-04-19 10:00:48 +0600

    Implement GitHub login system

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,4 +1,4 @@
-import { getAuth, signInWithPopup, signOut } from "firebase/auth";
+import { getAuth, signInWithPopup, GithubAuthProvider, signOut } from "firebase/auth";
 import app from "../../firebase/firebase.init";
 import { GoogleAuthProvider } from "firebase/auth";
 import { useState } from "react";
@@ -7,9 +7,23 @@ const Login = () => {
   const [user, setUser] = useState(null);
   const auth = getAuth(app);
   const provider = new GoogleAuthProvider();
+  const gitProvider = new GithubAuthProvider();
 
   const handleGoogleAuthProvider = () => {
     signInWithPopup(auth, provider)
+      .then((result) => {
+        const loggedInUser = result.user;
+        //stored user in a state . so you can easily show it in UI
+        setUser(loggedInUser);
+        console.log(loggedInUser);
+      })
+      .catch((error) => {
+        console.log("error", error.message);
+      });
+  };
+
+  const handleGitHubAuthProvider = () => {
+    signInWithPopup(auth, gitProvider)
       .then((result) => {
         const loggedInUser = result.user;
         //stored user in a state . so you can easily show it in UI
@@ -39,15 +53,21 @@ const Login = () => {
             Sign Out
           </button>
         ) : (
-          <button onClick={handleGoogleAuthProvider} className="p-3 border-2 border-black hover:bg-blue-500  rounded-lg text-black hover:text-white text-xl">
-            Google Login
-          </button>
+          <div className=" flex gap-5">
+            <button onClick={handleGoogleAuthProvider} className="p-3 border-2 border-black hover:bg-blue-500  rounded-lg text-black hover:text-white text-xl">
+              Google Login
+            </button>
+
+            <button onClick={handleGitHubAuthProvider} className="p-3 border-2 border-black hover:bg-blue-500  rounded-lg text-black hover:text-white text-xl">
+              GitHub Login
+            </button>
+          </div>
         )}
       </div>
 
       {user && (
         <div className="text-xl flex gap-3 border-2 rounded-lg border-green-500 p-5">
-          <img className="rounded-lg" src={user.photoURL} alt="user image" />
+          <img className="rounded-lg  w-40" src={user.photoURL} alt="user image" />
           <div className="">
             <h3 className="text-2xl">User: {user.displayName}</h3>
             <h3 className="text-2xl">Email: {user.email}</h3>
